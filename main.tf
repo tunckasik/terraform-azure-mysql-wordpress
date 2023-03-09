c6c981acfad4058aa6b30fe758feba9d6775e621
ccseyhan <cemalcan.seyhan@deop.ca>
2023-03-09 14:42:47 -0300

    provider added

--- main.tf
+++ main.tf
@@ -1,3 +1,18 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "3.46.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {
+    
+  }
+}
+
 resource "azurerm_resource_group" "rg" {
   name     = "example-resources"
   location =  var.location
